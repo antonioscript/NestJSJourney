ba493ccfa7c3f5577d28aab39e40249e8aee99b8
Antônio Rocha <antoniojunior159@gmail.com>
2024-05-14 00:13:58 -0300

    Commit

--- Node/00.ZeroToHero/server.js
+++ Node/00.ZeroToHero/server.js
@@ -1,9 +1,11 @@
 import { createServer } from 'node.http'
 
 const server = createServer(() => {
-    console.log('Hello Word Node');
+    console.log('Hello Word Node!');
 })
 
 server.listen(3333);
+
+//00. Para iniciar no terminal: npm -init -y
 
 
